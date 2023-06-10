597bd464270a62d96a4c711e8421975f7d0df39a
pabcrudel <pabcrudel@gmail.com>
2023-06-10 20:27:58 +0200

    Juego modo controlador funcionando

--- src/version_1/Blackjack.java
+++ src/version_1/Blackjack.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Blackjack {
-//	private Baraja baraja = new Baraja();
+	//	private Baraja baraja = new Baraja();
 	private List<Carta> baraja;
 	private List<Carta> manoJugador = new ArrayList<>();
 	private List<Carta> manoServidor = new ArrayList<>();
@@ -13,21 +13,21 @@ public class Blackjack {
 	public Blackjack() {
 		baraja = new ArrayList<>();
 
-        String[] palos = {"Corazones", "Diamantes", "Picas", "Tréboles"};
-        String[] valores = {"As", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jota", "Reina", "Rey"};
+		String[] palos = {"Corazones", "Diamantes", "Picas", "Treboles"};
+		String[] valores = {"As", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jota", "Reina", "Rey"};
 
-        for (String palo : palos) {
-            for (String valor : valores) {
-                Carta carta = new Carta(palo, valor);
-                baraja.add(carta);
-            }
-        }
+		for (String palo : palos) {
+			for (String valor : valores) {
+				Carta carta = new Carta(palo, valor);
+				baraja.add(carta);
+			}
+		}
 	}
-	
+
 	public List<Carta> getBaraja() {
 		return baraja;
 	}
-	
+
 	public void setBaraja(List<Carta> nuevaBaraja) {
 		baraja = nuevaBaraja;
 	}
@@ -40,7 +40,6 @@ public class Blackjack {
 		return manoServidor;
 	}
 
-
 	public int getValorJugador() {
 		return calcularValorMano(manoJugador);
 	}
@@ -48,93 +47,99 @@ public class Blackjack {
 	public int getValorServidor() {
 		return calcularValorMano(manoServidor);
 	}
-	
+
 	public void barajar() {
 		Collections.shuffle(baraja);
 	}
-	
-    public void repartirCarta(boolean isServer) {
-    	Carta carta = baraja.remove(baraja.size() - 1);
-    	
-    	if (isServer) {
-    		manoServidor.add(carta);
-    	}
-    	else {
-    		manoJugador.add(carta);
-    	}
-    }
 
+	public void repartirCarta(boolean isServer) {
+		Carta carta = baraja.remove(baraja.size() - 1);
 
-//	public void jugarTurno() {
-//		Carta carta = baraja.repartirCarta();
-//		manoJugador.add(carta);
-//	}
-//
-//	public void jugarTurnoServidor() {
-//		Carta carta = baraja.repartirCarta();
-//		manoServidor.add(carta);
-//
-//	}
-
-
-public boolean jugadorGana() {
-	int valorJugador = getValorJugador();
-	int valorServidor = getValorServidor();
-
-	return valorJugador <= 21 && (valorJugador > valorServidor || valorServidor > 21);
-}
-
-public boolean servidorGana() {
-	int valorJugador = getValorJugador();
-	int valorServidor = getValorServidor();
-
-	return valorServidor <= 21 && (valorServidor > valorJugador || valorJugador > 21);
-}
-
-public boolean jugadorSePasa() {
-	return getValorJugador() > 21;
-}
-
-public boolean ServidorSePasa() {
-	return getValorServidor() > 21;
-}
-
-//public void reiniciar() {
-//	baraja = new Baraja();
-//	baraja.barajar();
-//
-//	manoJugador.clear();
-//	manoServidor.clear();
-//
-//	repartirCartasIniciales();
-//}
-
-//private void repartirCartasIniciales() {
-//	for (int i = 0; i < 2; i++) {
-//		Carta cartaJugador = baraja.repartirCarta();
-//		Carta cartaServidor = baraja.repartirCarta();
-//
-//		manoJugador.add(cartaJugador);
-//		manoServidor.add(cartaServidor);
-//	}
-//}
-
-private int calcularValorMano(List<Carta> mano) {
-	int valor = 0;
-	int ases = 0;
-
-	for (Carta carta : mano) {
-		valor += carta.getValorNumerico();
-		if (carta.getValor().equals("As")) {
-			ases++;
+		if (isServer) {
+			manoServidor.add(carta);
+			if (ServidorSePasa()) {
+				jugadorGana();
+			}
+		}
+		else {
+			manoJugador.add(carta);
+			if (jugadorSePasa()) {
+				servidorGana();
+			}
 		}
 	}
 
-	while (valor > 21 && ases > 0) {
-		valor -= 10;
-		ases--;
+
+	//	public void jugarTurno() {
+	//		Carta carta = baraja.repartirCarta();
+	//		manoJugador.add(carta);
+	//	}
+	//
+	//	public void jugarTurnoServidor() {
+	//		Carta carta = baraja.repartirCarta();
+	//		manoServidor.add(carta);
+	//
+	//	}
+
+
+	public boolean jugadorGana() {
+		int valorJugador = getValorJugador();
+		int valorServidor = getValorServidor();
+
+		return valorJugador <= 21 && (valorJugador > valorServidor || valorServidor > 21);
 	}
 
-	return valor;
-}
+	public boolean servidorGana() {
+		int valorJugador = getValorJugador();
+		int valorServidor = getValorServidor();
+
+		return valorServidor <= 21 && (valorServidor > valorJugador || valorJugador > 21);
+	}
+
+	public boolean jugadorSePasa() {
+		return getValorJugador() > 21;
+	}
+
+	public boolean ServidorSePasa() {
+		return getValorServidor() > 21;
+	}
+
+	//public void reiniciar() {
+	//	baraja = new Baraja();
+	//	baraja.barajar();
+	//
+	//	manoJugador.clear();
+	//	manoServidor.clear();
+	//
+	//	repartirCartasIniciales();
+	//}
+
+	//private void repartirCartasIniciales() {
+	//	for (int i = 0; i < 2; i++) {
+	//		Carta cartaJugador = baraja.repartirCarta();
+	//		Carta cartaServidor = baraja.repartirCarta();
+	//
+	//		manoJugador.add(cartaJugador);
+	//		manoServidor.add(cartaServidor);
+	//	}
+	//}
+
+	private int calcularValorMano(List<Carta> mano) {
+		int valor = 0;
+		int ases = 0;
+
+		for (Carta carta : mano) {
+			valor += carta.getValorNumerico();
+			if (carta.getValor().equals("As")) {
+				ases++;
+			}
+		}
+
+		while (valor > 21 && ases > 0) {
+			valor -= 10;
+			ases--;
+		}
+
+		return valor;
+	}
 }
--- src/version_1/Controlador.java
+++ src/version_1/Controlador.java
@@ -3,6 +3,7 @@ package version_1;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -19,6 +20,9 @@ public class Controlador {
 	private static String IP = "127.0.0.1";
 
 	private Blackjack blackjack = new Blackjack();
+	private int puntuacionContrario;
+
+	private static Scanner scanner = new Scanner(System.in);
 
 	private void iniciarServidor() throws IOException {
 		serverSocket = new ServerSocket(PORT);
@@ -51,17 +55,97 @@ public class Controlador {
 
 		blackjack.setBaraja(otraBaraja);
 	}
-	
+
+	private void leerResultado() throws ClassNotFoundException, IOException {
+		Object ob = inputStream.readObject();
+
+		Integer res = (Integer) ob;
+
+		puntuacionContrario = res.intValue();
+	}
+
 	private void enviarBaraja() throws IOException {
 		outputStream.writeObject(blackjack.getBaraja());
 	}
-	
+
+	private void enviarResultado() throws IOException {
+		Integer res = null;
+
+		if (isServer) {
+			res = Integer.valueOf(blackjack.getValorServidor());
+		}
+		else {
+			res = Integer.valueOf(blackjack.getValorJugador());
+		}
+
+		outputStream.writeObject(res);
+	}
+
 	private void repartirCarta() {
 		blackjack.repartirCarta(isServer);
 	}
 
+	public void mostrarDatos() {
+		String datos;
+
+		if (isServer) {
+			datos = blackjack.getManoServidor().toString() + "\n";
+			datos += "Valor: " + blackjack.getValorServidor();
+		}
+		else {
+			datos = blackjack.getManoJugador().toString() + "\n";
+			datos += "Valor: " + blackjack.getValorJugador();
+		}
+
+		System.out.println(datos);
+	}
+
+	public int getValor() {
+		int valor;
+
+		if (isServer) {
+			valor = blackjack.getValorServidor();
+		}
+		else {
+			valor = blackjack.getValorJugador();
+		}
+
+		return valor;
+	}
+
+	private void jugar() throws IOException {
+		boolean plantarse = false;
+
+		while(!plantarse) {
+			System.out.println("Que deseas hacer? (1: Pedir carta, 2: Plantarse)");
+			int opcion = scanner.nextInt();
+
+			if (opcion == 1) {
+				repartirCarta();
+
+				mostrarDatos();
+				if (getValor() > 21) {
+					plantarse = true;
+					System.out.println("Has perdido");
+				}
+			}
+			else plantarse = true;
+		}
+	}
+
+	public void getGanador() {
+		if (puntuacionContrario <= 21 && (getValor() > 21 || puntuacionContrario > getValor())) {
+			System.out.println("Gana tu oponente");
+		}
+		else if (getValor() <= 21 && (puntuacionContrario > 21 || getValor() > puntuacionContrario)) {
+			System.out.println("Ganaste tu");
+		}
+		else {
+			System.out.println("Nadie gana");
+		}
+	}
+
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 		System.out.print("Elige el modo de juego (1: Servidor, 2: Cliente): ");
 		int opcion = scanner.nextInt();
 
@@ -75,6 +159,7 @@ public class Controlador {
 
 				for (int i = 0; i < 2; i++)
 					controlador.repartirCarta();
+				controlador.mostrarDatos();
 
 				controlador.enviarBaraja();
 				controlador.leerBaraja();
@@ -87,10 +172,26 @@ public class Controlador {
 
 				for (int i = 0; i < 2; i++) 
 					controlador.repartirCarta();
+				controlador.mostrarDatos();
 
 				controlador.enviarBaraja();
 				break;
 			}
+
+			if (controlador.isServer) {
+				controlador.jugar();
+				controlador.enviarBaraja();
+				controlador.leerResultado();
+				controlador.enviarResultado();
+			}
+			else {
+				controlador.leerBaraja();
+				controlador.jugar();
+				controlador.enviarResultado();
+				controlador.leerResultado();
+			}
+
+			controlador.getGanador();
 		} 
 		catch (IOException e) {
 			e.printStackTrace();
